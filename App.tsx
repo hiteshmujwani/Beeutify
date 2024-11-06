8298d2d62442e3b98512b688f62bded23c34b46b
hiteshmujwani <hiteshmujwaniofficial@gmail.com>
2024-11-06 15:34:15 +0530

    added splash screen and app icon

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import {Navigation} from './src/navigation/Navigation';
 import { Provider } from 'react-redux';
 import store from './store/store';
 
+
 const App = () => {
   return (
     <Provider store={store}>
