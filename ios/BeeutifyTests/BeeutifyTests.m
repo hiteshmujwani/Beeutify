57d1eb048caeacac47da06e2a23f226643da5d67
Surya Subhrajit <suryasubhrajit@gmail.com>
2024-11-13 20:14:25 +0530

    change name spaces to Beeutify in ios folder

--- ios/BeeutifyTests/BeeutifyTests.m
+++ ios/BeeutifyTests/BeeutifyTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface ShaadowTests : XCTestCase
+@interface BeeutifyTests : XCTestCase
 
 @end
 
-@implementation ShaadowTests
+@implementation BeeutifyTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
